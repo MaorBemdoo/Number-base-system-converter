524227b8e6e307c2a8ce6e125a5fcb81383deef6
Bemdoo Maor <bemdoo.maor1@gmail.com>
2024-12-10 10:31:28 +0100

    chore: update hexdecimal to octal and vice

--- lib/converters/octal_to_hexadecimal.py
+++ lib/converters/octal_to_hexadecimal.py
@@ -1,19 +1,21 @@
-def octal_to_hexadecimal(number):
+def octal_to_hexadecimal(octal):
     hex_dictionary = {
         0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5",
         6: '6', 7: "7", 8: "8", 9: "9", 10: "A", 11: "B",
         12: "C", 13: "D", 14: "E", 15: "F",
     }
 
+    octal = int(octal)
+
     decimal = 0
     base_num = 1
-    while number > 0:
-        remainder = number % 10
+    while octal > 0:
+        remainder = octal % 10
         decimal += remainder * base_num
-        number //= 10
+        octal //= 10
         base_num *= 8
 
-    hexadecimal = "0"
+    hexadecimal = ""
     while decimal > 0:
         remainder = decimal % 16
         hexadecimal = hex_dictionary[remainder] + hexadecimal
