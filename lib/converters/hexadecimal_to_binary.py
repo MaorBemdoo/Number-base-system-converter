1861c24dc753ada15967cd3c6fe47babfe2fb0ac
Praise Martins <praise.martins@binghamuni.edu.ng>
2024-12-11 10:25:42 +0100

    hexadecimal to decimal converter

---
+++ lib/converters/hexadecimal_to_binary.py
@@ -0,0 +1,15 @@
+def hexadecimal_to_binary(hex_num):
+    hexadecimal_dict = {
+        '0': '0000', '1': '0001', '2': '0010', '3': '0011', '4': '0100', '5': '0101',
+        '6': '0110', '7': '0111', '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
+        'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
+    }
+
+    hex_num = hex_num.upper()
+    binary = ""
+
+    for digit in hex_num:
+        binary += hexadecimal_dict[digit]
+
+    binary = binary.lstrip("0")
+    return binary
