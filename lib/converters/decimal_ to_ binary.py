d42cd244e786bcf8963a15cbf63ef5c9dd7c6737
Kersedoo <160669809+Kersedoo@users.noreply.github.com>
2024-12-12 14:02:52 +0100

    Update decimal_ to_ binary.py

---
+++ lib/converters/decimal_ to_ binary.py
@@ -0,0 +1,10 @@
+def decimal_to_binary(decimal_number):
+    if decimal_number == 0:
+        return "0"
+    binary = ""
+    while decimal_number > 0:
+        binary = str(decimal_number % 2) + binary 
+        decimal_number //= 2
+
+    return binary
+    
