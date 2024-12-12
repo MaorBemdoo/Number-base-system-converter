f32e4d60675b4b5ab906f15ac3115fa0bb226ac5
davidcodes <122282617+aiyejinadavid@users.noreply.github.com>
2024-12-12 11:07:04 +0100

    i  created the binary to decimal converter

---
+++ binary_to_decimal.py
@@ -0,0 +1,12 @@
+def binary_to_decimal(binary):
+    decimal = 0
+    i = 0
+    while binary != 0:
+        rem = binary % 10
+        decimal = decimal + rem * pow(2, i)
+        binary = binary // 10
+        i += 1
+
+    return decimal
+
+
