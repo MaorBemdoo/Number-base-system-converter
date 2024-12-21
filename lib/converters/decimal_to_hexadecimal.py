a3c68f2ad9402a3e370a4acd5ded8f2aec02eab4
Nomte <150367876+Nomte1104@users.noreply.github.com>
2024-12-21 23:02:21 +0000

    DECIMAL TO HEXADECIMAL

---
+++ lib/converters/decimal_to_hexadecimal.py
@@ -0,0 +1,10 @@
+def decimal_to_hexadecimsal(decimal):
+    hexadecimal = ""
+    hex_digits = "0123456789ABCDEF"
+    while decimal > 0:
+        remainder = int(decimal) % 16
+        hexadecimal = hex_digits[remainder]+hexadecimal
+        decimal //= 16
+    return hexadecimal
+
+print(decimal_to_hexadecimsal(88))
